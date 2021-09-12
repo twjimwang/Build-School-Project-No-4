295a043cf5b3dce69cf7bdfc867bad714d4fa396
Kenny <wymloveocean@gmail.com>
2021-09-12 15:51:21 +0800

    手機板分頁超連結設定

---
+++ Build-School-Project-No-4/Controllers/MeForPhoneController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Build_School_Project_No_4.Controllers
+{
+    public class MeForPhoneController : Controller
+    {
+        // GET: MeForPhone
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MeForPhonePage()
+        {
+            return View();
+        }
+    }
+}
