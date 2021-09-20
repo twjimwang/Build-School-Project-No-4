17c46537580ea64e4bfdce609ffe265f77dafaf3
Johnson <johnson1990hitachi@outlook.com>
2021-09-20 23:06:29 +0800

    新增ChillMeetLike讀資料

---
+++ Build-School-Project-No-4/Controllers/ChillMeetLikeController.cs
@@ -0,0 +1,26 @@
+﻿using Build_School_Project_No_4.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Build_School_Project_No_4.Controllers
+{
+    public class ChillMeetLikeController : Controller
+    {
+        // GET: ChillMeetLike
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MeetLikes()
+        {
+            var memberGet = new MemberService();
+            var members = memberGet.GetMember();
+
+            return View(members);
+        }
+    }
+}
