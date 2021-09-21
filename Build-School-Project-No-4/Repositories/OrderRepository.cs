80480099b7b773eea5fdf704aec66b29a42b551d
kyle <kyle60902@gmail.com>
2021-09-21 21:03:58 +0800

    0921kyle_Codefirst_ReadDatabase

---
+++ Build-School-Project-No-4/Repositories/OrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using Build_School_Project_No_4.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Build_School_Project_No_4.Repositories
+{
+    public class OrderRepository
+    {
+        EPalContext _ctx;
+        public OrderRepository()
+        {
+            _ctx = new EPalContext();
+        }
+
+        public void WriteDataToDB()
+        {
+            using (EPalContext _ctx = new EPalContext())
+            {
+                List<Order> Order = new List<Order>
+                {
+                   new Order { OrderId = 1, PlayerId = 2,OrderStatus="" ,OrderDate =  DateTime.ParseExact("2021-06-03", "yyyy-MM-dd", null),Quality =1,UnitPrice=30 },
+                      
+                };
+                _ctx.Orders.AddRange(Order);
+                _ctx.SaveChanges();
+            }
+        }
+
+        public List<Order> ReadOrder()
+        {
+            return _ctx.Orders.ToList();
+        }
+    }
+}
---
+++ Build-School-Project-No-4/Services/OrderService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Build_School_Project_No_4.DataModels;
+using Build_School_Project_No_4.Repositories;
+using Build_School_Project_No_4.ViewModels;
+
+namespace Build_School_Project_No_4.Services
+{
+    public class OrderService
+    {
+        private OrderRepository _OrderRepo;
+        public OrderService()
+        {
+            _OrderRepo = new OrderRepository();
+        }
+
+        public List<OrderViewModel> Order()
+        {
+            List<Order> Orders = _OrderRepo.ReadOrder();
+
+            List<OrderViewModel> result = new List<OrderViewModel>();
+            foreach (var item in Orders)
+            {
+                result.Add(new OrderViewModel
+                {
+                    OrderStatus = item.OrderStatus,
+                    OrderId = item.OrderId,
+                    PlayerId = item.PlayerId,
+                    OrderDate = item.OrderDate,
+                    Quality = item.Quality,
+                    UnitPrice = item.UnitPrice,
+                    TotalPrice = item.Quality * item.UnitPrice
+                });
+            }
+            return result;
+        }
+       
+    }
+}
