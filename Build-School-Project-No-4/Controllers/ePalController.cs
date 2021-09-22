d877edc5cf0d122a47014e85a2ecbd9764ed4fba
唐樂維 <le.kentang@gmail.com>
2021-09-22 10:59:38 +0800

    add ePal 五權分立

--- Build-School-Project-No-4/Controllers/ePalController.cs
+++ Build-School-Project-No-4/Controllers/ePalController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Build_School_Project_No_4.Services;
 
 namespace Build_School_Project_No_4.Controllers
 {
@@ -12,6 +13,13 @@ namespace Build_School_Project_No_4.Controllers
         public ActionResult ePal()
         {
             return View();
+        }
+        public ActionResult EPalIndex()
+        {
+            var productGet = new ProductService();
+            var abc = productGet.GetProductData();
+            return View(abc);
+            
         }
     }
 }
---
+++ Build-School-Project-No-4/Repositories/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using Build_School_Project_No_4.DataModels;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Build_School_Project_No_4.Services
+{
+    public class ProductRepository
+    {
+        private readonly DbContext _context;
+
+        public ProductRepository()
+        {
+            _context = new EPalContext();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+        public void Create<T>(T value) where T : class
+        {
+            _context.Entry(value).State = EntityState.Added;
+        }
+        public void Update<T>(T value) where T : class
+        {
+            _context.Entry(value).State = EntityState.Modified;
+        }
+        public void Delete<T>(T value) where T : class
+        {
+            _context.Entry(value).State = EntityState.Deleted;
+        }
+        //public IQueryable<T> GetAll<T, Tkey>(Expression<Func<T, Tkey>> keyselector) where T : class
+        //{
+        //    return _context.Set<T>().OrderBy(keyselector);
+        //}
+        public IQueryable<T> GetAll<T>() where T : class
+        {
+            return _context.Set<T>();
+        }
+    }
+}
