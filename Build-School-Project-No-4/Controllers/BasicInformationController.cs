fce873cdb763527da58dd4f35a07e0b4f3dde079
Lesley Pan <lesley.jilli1215@gmail.com>
2021-09-27 19:31:49 +0800

    commit

---
+++ Build-School-Project-No-4/Controllers/BasicInformationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Build_School_Project_No_4.Controllers
+{
+    public class BasicInformationController : Controller
+    {
+        // GET: BasicInformation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BasicInformation()
+        {
+            return View();
+        }
+    }
+}
