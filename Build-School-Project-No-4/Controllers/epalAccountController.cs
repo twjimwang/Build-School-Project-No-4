d9bde208a096f4609f8d7da3f89a4a3dab5ea2e5
kyle <kyle60902@gmail.com>
2021-10-07 21:28:47 +0800

    10/07/WalletRead

--- Build-School-Project-No-4/Controllers/epalAccountController.cs
+++ Build-School-Project-No-4/Controllers/epalAccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Build_School_Project_No_4.Services;
+using Build_School_Project_No_4.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,8 +19,13 @@ namespace Build_School_Project_No_4.Controllers
         public ActionResult Wallet()
         {
             var Wallet = new WalletService();
+            var Wallets = Wallet.GetWalletData();
 
-            return View();
+            GroupViewModel Epalindex = new GroupViewModel
+            {
+                wallets = Wallets
+            };
+            return View(Epalindex);
         }
 
       
